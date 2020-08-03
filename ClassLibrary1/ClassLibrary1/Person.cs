6de9250a2ef70340c56fa9af1daf35ac74a5db07
René J <shiomelol@live.dk>
2020-08-03 13:44:40 +0200

    Library classes
    Small console app with custom form for writing to lists for library testing

---
+++ ClassLibrary1/ClassLibrary1/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public Job job;
+
+        public Person(string name, int age, Job job)
+        {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+        }
+    }
+}
---
+++ ClassLibrary1/LibraryClassTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personRegister = new List<Person>();
+            List<Job> jobRegister = new List<Job>();
+            string confirm;
+            bool isRunning = true;
+
+
+            // Console form for testing the list builder
+            Console.WriteLine("Form builder test \n");
+            while (isRunning == true)
+            {
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter Job title: ");
+                string title = Console.ReadLine();
+
+                Console.Write("Enter Job salary: ");
+                double payment = double.Parse(Console.ReadLine());
+
+                Job job = new Job(title, payment);
+                Person person = new Person(name, age, job);
+                jobRegister.Add(job);
+                personRegister.Add(person);
+
+                Console.Write("Add another entry? ");
+                confirm = Console.ReadLine();
+                Console.WriteLine();
+
+                if(confirm.ToLower() == "n")
+                {
+                    isRunning = false;
+                }
+
+            }
+            for (int i = 0; i < personRegister.Count; i++)
+            {
+                Console.WriteLine($"{personRegister[i].name} {personRegister[i].age}");
+                Console.WriteLine($"{jobRegister[i].title} {jobRegister[i].salary} \n");
+            }
+
+        }
+    }
+}
