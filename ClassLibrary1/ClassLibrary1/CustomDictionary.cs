d7c39d75afdd6685e2803a8a6c07ac09edc56bd2
René J <shiomelol@live.dk>
2020-08-04 14:17:21 +0200

    Solution commit partly failed

--- ClassLibrary1/ClassLibrary1/CustomDictionary.cs
+++ ClassLibrary1/ClassLibrary1/CustomDictionary.cs
@@ -7,9 +7,9 @@ namespace ClassLibrary1
     public class CustomDictionary
     {
 
-        private Dictionary<string, int> opgave1 = new Dictionary<string, int>();
-        private Dictionary<float, bool> opgave2 = new Dictionary<float, bool>();
-        private Dictionary<Person, int> personDic = new Dictionary<Person, int>();
+        private Dictionary<string, int> opgave1;
+        private Dictionary<float, bool> opgave2;
+        private Dictionary<Person, int> personDic;
 
         public Dictionary<string, int> Opgave1()
         {
