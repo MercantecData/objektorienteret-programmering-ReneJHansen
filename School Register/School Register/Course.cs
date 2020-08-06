8cf53ff72a70548f3639a4d0eaa27e1f0cecd5ae
René J <shiomelol@live.dk>
2020-08-06 14:32:04 +0200

    School register (home assignment)

---
+++ School Register/School Register/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School_Register
+{
+    public class Course
+    {
+        public int id;
+        public string courseName;
+        public int students;
+        public string teacher;
+        public List<Student> enrolledList = new List<Student>();
+
+        public Course(int id, string courseName, int students, string teacher, List<Student> enrolledList)
+        {
+            this.id = id;
+            this.courseName = courseName;
+            this.students = students;
+            this.teacher = teacher;
+            this.enrolledList = enrolledList;
+        }
+
+        public List<Student> GetStudents()
+        {
+            return enrolledList;
+        }
+    }
+}
---
+++ School Register/School Register/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School_Register
+{
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public bool isTeacher;
+        public float salary;
+        public bool paidThisMonth;
+        public List<Course> courses = new List<Course>();
+
+        public Employee(int id, string name, bool isTeacher, float salary, bool paidThisMonth)
+        {
+            this.id = id;
+            this.name = name;
+            this.isTeacher = isTeacher;
+            this.salary = salary;
+            this.paidThisMonth = paidThisMonth;
+        }
+
+        public List<Course> GetCourses()
+        {
+            return courses;
+        }
+
+        public bool ReceivedPay()
+        {
+            return paidThisMonth;
+        }
+    }
+}
