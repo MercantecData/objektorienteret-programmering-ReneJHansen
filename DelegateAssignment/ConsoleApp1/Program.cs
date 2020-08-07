902c9ead25bf1cee39821d9f7ee57af8f750cd14
René J <shiomelol@live.dk>
2020-08-07 10:56:57 +0200

    Delegate assignment minus the bonus in Math class

---
+++ DelegateAssignment/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using DelegateAssignment;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave 1
+            DelegateTesting opgave1 = new DelegateTesting();
+            Opgave1Delegate emptyDel;
+
+            emptyDel = opgave1.Opgave1Test;
+
+            emptyDel();
+
+
+            // Opgave 2
+            DelegateTesting opgave2 = new DelegateTesting();
+            Opgave2Delegate emptyDel2;
+
+            emptyDel2 = opgave2.Opgave2Test;
+
+            emptyDel2();
+            Console.WriteLine(emptyDel2().ToString());
+
+
+            // Opgave 3
+            DelegateTesting opgave3 = new DelegateTesting();
+            Opgave3Delegate emptyDel3;
+
+            emptyDel3 = opgave3.Opgave3Test;
+
+            emptyDel3("This is", "three", "strings");
+        }
+    }
+}
---
+++ DelegateAssignment/DelegateAssignment/DelegateTesting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateAssignment
+{
+    public delegate void Opgave1Delegate();
+    public delegate float Opgave2Delegate();
+    public delegate void Opgave3Delegate(string a, string b, string c);
+    public class DelegateTesting
+    {
+        public void Opgave1Test()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public float Opgave2Test()
+        {
+            return 4.2f;
+        }
+
+        public void Opgave3Test(string a, string b, string c)
+        {
+            Console.WriteLine($"{a} {b} {c}");
+        }
+    }
+}
