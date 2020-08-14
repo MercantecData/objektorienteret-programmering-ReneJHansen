52606cc65b507d9ac49fea624ddda3a740c7da18
René J <shiomelol@live.dk>
2020-08-14 11:39:50 +0200

    Præsentation

--- DelegateAssignment/ConsoleApp1/Program.cs
+++ DelegateAssignment/ConsoleApp1/Program.cs
@@ -8,29 +8,27 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Opgave 1
-            DelegateTesting opgave1 = new DelegateTesting();
+            DelegateTesting opgave = new DelegateTesting();
             Opgave1Delegate emptyDel;
 
-            emptyDel = opgave1.Opgave1Test;
+            emptyDel = opgave.Opgave1Test;
 
             emptyDel();
 
 
             // Opgave 2
-            DelegateTesting opgave2 = new DelegateTesting();
             Opgave2Delegate emptyDel2;
 
-            emptyDel2 = opgave2.Opgave2Test;
+            emptyDel2 = opgave.Opgave2Test;
 
             emptyDel2();
             Console.WriteLine(emptyDel2().ToString());
 
 
             // Opgave 3
-            DelegateTesting opgave3 = new DelegateTesting();
             Opgave3Delegate emptyDel3;
 
-            emptyDel3 = opgave3.Opgave3Test;
+            emptyDel3 = opgave.Opgave3Test;
 
             emptyDel3("This is", "three", "strings");
         }
